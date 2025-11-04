4c272dc4ca7ff34e3bbd060a199d0f0c3c37e143
way <way@default-10-231-129-203.vpnuser.dartmouth.edu>
2025-11-03 23:06:05 -0500

    fix bugs

--- src/open-r1-multimodal/src/open_r1/vlm_modules/__init__.py
+++ src/open-r1-multimodal/src/open_r1/vlm_modules/__init__.py
@@ -1,6 +1,11 @@
 from .vlm_module import VLMBaseModule
 from .qwen_module import Qwen2VLModule
 from .internvl_module import InvernVLModule
-from .glm_module import GLMVModule
 
-__all__ = ["VLMBaseModule", "Qwen2VLModule", "InvernVLModule","GLMVModule"]
+# Optional GLM support (requires newer transformers with Glm4vForConditionalGeneration)
+try:
+    from .glm_module import GLMVModule
+    __all__ = ["VLMBaseModule", "Qwen2VLModule", "InvernVLModule", "GLMVModule"]
+except ImportError:
+    GLMVModule = None
+    __all__ = ["VLMBaseModule", "Qwen2VLModule", "InvernVLModule"]
